076053863d00d3043a11825997e46b814af1af53
MD-KpRk <griferad@gmail.com>
2022-02-15 16:36:28 +0300

    Настроено отображение главного окна. Добавлен файл стилей. В файл стилей добавлено описание основных цветов

--- AutoRestProject/MainWindow.xaml.cs
+++ AutoRestProject/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,9 +16,6 @@ using System.Windows.Shapes;
 
 namespace AutoRestProject
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
